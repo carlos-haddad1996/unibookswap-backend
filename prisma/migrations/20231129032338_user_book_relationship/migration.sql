b5f8cdcb752becb5894b05fc13b7ab45a837dbfa
Carlor Haddad <carlos.haddad@partnerhero.com>
2023-11-28 23:59:56 -0600

    feat: added cloud storage and modified add book endpoints

---
+++ prisma/migrations/20231129032338_user_book_relationship/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "price" SET DATA TYPE TEXT;
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,52 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator';
+import { BookDto } from './book.dto';
+import { IsNumber } from 'class-validator';
 
-export class CreateBookDto {
-  @ApiProperty({
-    type: String,
-    description: `Product's title`,
-    example: 'Book 1',
-  })
-  @IsString()
-  title: string;
-
-  @ApiProperty({
-    type: String,
-    description: `Product's author`,
-    example: 'John Doe',
-  })
-  @IsString()
-  author: string;
-
+export class CreateBookDto extends BookDto {
   @ApiProperty({
     type: Number,
-    description: `Product's price`,
-    example: 30.4,
+    description: `User's title`,
+    example: 'Book 1',
   })
   @IsNumber()
-  price: number;
-
-  @ApiProperty({
-    type: String,
-    description: `Product's description`,
-    example: 'This is book 1',
-  })
-  @IsString()
-  description: string;
-
-  @ApiProperty({
-    type: String,
-    description: `Product's category`,
-    example: 'Science & Fiction',
-  })
-  @IsString()
-  category: string;
-
-  @ApiProperty({
-    type: String,
-    description: `Product's image`,
-    example: 'Book 1 image',
-  })
-  @IsString()
-  image: string;
+  userId: number;
 }
